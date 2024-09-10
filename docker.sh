ceef62c57e07cd051f1f049f560aa8f57ae6f846
sistracia <156994197+sistracia@users.noreply.github.com>
2024-09-10 19:58:14 +0700

    Publish Docker image to GitHub's Package

--- docker.sh
+++ docker.sh
@@ -47,6 +47,8 @@ else
 fi
 
 time docker build --rm=false \
+    --label runnumber=${GITHUB_RUN_ID} \
+    --label org.opencontainers.image.source=${IMAGE_SOURCE} \
     --build-arg ARCH=$ARCH \
     --build-arg ICONIFY_API_VERSION=${ICONIFY_API_VERSION} \
     --build-arg BUILD_DATE="$(date +"%Y-%m-%dT%H:%M:%SZ")" \
